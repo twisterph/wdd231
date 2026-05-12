71a2c2124ae4a907a803e7f06e51ef849dec2141
Pablo Hinojosa <phinojosaiturra@byupathway.edu>
2026-05-11 22:16:31 -0700

    w2 learning act5

---
+++ scripts/prophets.js
@@ -0,0 +1,63 @@
+const url =
+'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+
+    prophets.forEach((prophet) => {
+
+        let card = document.createElement('section');
+
+        let fullName = document.createElement('h2');
+
+        let birthDate = document.createElement('p');
+
+        let birthPlace = document.createElement('p');
+
+        let portrait = document.createElement('img');
+
+        fullName.textContent =
+            `${prophet.name} ${prophet.lastname}`;
+
+        birthDate.innerHTML =
+            `<strong>Date of Birth:</strong> ${prophet.birthdate}`;
+
+        birthPlace.innerHTML =
+            `<strong>Place of Birth:</strong> ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+
+        portrait.setAttribute(
+            'alt',
+            `Portrait of ${prophet.name} ${prophet.lastname}`
+        );
+
+        portrait.setAttribute('loading', 'lazy');
+
+        portrait.setAttribute('width', '340');
+
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+
+        card.appendChild(birthDate);
+
+        card.appendChild(birthPlace);
+
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData();
